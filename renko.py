212b62fe54cb606fed3fd9c7495139ae8308774a
maxoja <tawan.tpptnk@gmail.com>
2020-07-26 00:33:18 +0100

    Cleanup renko.py

--- renko.py
+++ renko.py
@@ -3,6 +3,6 @@ DOWN_BOX = '-'
 
 def loadSequence(quote, boxSize):
     with open(f'./data/{quote}_{boxSize}.txt', "rb") as file:
-        header = file.read(2)
+        _ = file.read(2)
         content = file.read().decode('utf-16-le').split('\n')[2].strip()
         return content
