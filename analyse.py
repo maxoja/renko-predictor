a3e802891fbeae1c637a25d655aa359fff274407
maxoja <tawan.tpptnk@gmail.com>
2020-07-26 01:00:11 +0100

    Extract function for creating stats book

--- analyse.py
+++ analyse.py
@@ -4,9 +4,7 @@ from stats import KnowledgeBook
 FILE_NAME = 'eurgbp_50.txt'
 FUTURE_LEN = 1
 
-if __name__ == '__main__':
-    pastLen = int(input())
-    futureLen = FUTURE_LEN
+def createBook(filename, pastLen, futureLen):
     blockLen = pastLen + futureLen
     renko = loadSequence(FILE_NAME)
     book = KnowledgeBook()
@@ -23,3 +21,7 @@ if __name__ == '__main__':
         book.includeSample(featurePattern, futurePattern)
 
     book.showAllInfo()
+    return book
+
+if __name__ == '__main__':
+    book = createBook(FILE_NAME, int(input()), FUTURE_LEN)
