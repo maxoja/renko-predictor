14de25ea673a9965e64f48b84c10b05facf608b8
maxoja <tawan.tpptnk@gmail.com>
2020-07-26 02:44:38 +0100

    Create crude state/action traversal functions

--- analyse.py
+++ analyse.py
@@ -1,5 +1,5 @@
 from renko import loadSequence
-from stats import KnowledgeBook
+from stats import KnowledgeBook, State, Action, POSITION_NONE
 
 FILE_NAME = 'eurgbp_50.txt'
 FUTURE_LEN = 1
@@ -23,5 +23,35 @@ def craftBook(filename, pastLen, futureLen):
     book.showAllInfo()
     return book
 
+
+def recurseState(state:State, depthTillStop:int):
+
+    print(f'state {state.pattern} {state.position}')
+
+    if depthTillStop > 0:
+        for action in state.actions:
+            recurse(action, depthTillStop-1)
+
+
+def recurseAction(action:Action, depthTillStop:int):
+
+    print(f'action {action.fromState} {action.type}')
+
+    if depthTillStop > 0:
+        for state, prob in action.outcomes.items():
+            recurse(state, depthTillStop-1)
+
+
+def recurse(stateOrAction, depthTillStop=1):
+
+    if isinstance(stateOrAction, State):
+        recurseState(stateOrAction, depthTillStop)
+
+    if isinstance(stateOrAction, Action):
+        recurseAction(stateOrAction, depthTillStop)
+
+
 if __name__ == '__main__':
     book = craftBook(FILE_NAME, int(input()), FUTURE_LEN)
+    startState = State.create(book, "++", POSITION_NONE)
+    recurse(startState, 5)
